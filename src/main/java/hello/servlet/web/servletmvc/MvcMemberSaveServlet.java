fcbfa25efbba779b1203e0eb642b220669a95302
yh725 <yh725@kakao.com>
2023-02-18 23:53:57 +0900

    서블릿, JSP, MVC 패턴 - MVC패턴(개요) - MVC패턴(적용) - MVC패턴(한계)

--- src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
+++ src/main/java/hello/servlet/web/servletmvc/MvcMemberSaveServlet.java
@@ -1,47 +1,35 @@
-package hello.servlet.web;
+package hello.servlet.web.servletmvc;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet(name = "memberSaveServlet", urlPatterns = "/servlet/members/save")
-public class MemberSaveServlet extends HttpServlet {
+@WebServlet(name = "mvcMemberSaveServlet", urlPatterns = "/servlet-mvc/members/save")
+public class MvcMemberSaveServlet extends HttpServlet {
 
 	private MemberRepository memberRepository = MemberRepository.getInstance();
 
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		System.out.println("MemberSaveServlet.service");
 		String username = request.getParameter("username");
 		int age = Integer.parseInt(request.getParameter("age"));
 
 		Member member = new Member(username, age);
 		memberRepository.save(member);
 
-		response.setContentType("text/html");
-		response.setCharacterEncoding("utf-8");
-		PrintWriter w = response.getWriter();
-		w.write("<html>\n" +
-				"<head>\n" +
-				" <meta charset=\"UTF-8\">\n" +
-				"</head>\n" +
-				"<body>\n" +
-				"성공\n" +
-				"<ul>\n" +
-				" <li>id="+member.getId()+"</li>\n" +
-				" <li>username="+member.getUsername()+"</li>\n" +
-				" <li>age="+member.getAge()+"</li>\n" +
-				"</ul>\n" +
-				"<a href=\"/index.html\">메인</a>\n" +
-				"</body>\n" +
-				"</html>");
+		//Model에 데이터를 보관한다.
+		request.setAttribute("member", member);
+
+		String viewPath = "/WEB-INF/views/save-result.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+		dispatcher.forward(request, response);
 	}
 }
